4befb64bab3d8a7c17d9e30aad84cbb9bddeef00
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2026-02-12 15:01:25 +0100

    WIP

--- mpi_test.py
+++
@@ -1,9 +0,0 @@
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-print(f"Rank {rank} of {size}")
-total = comm.allreduce(1, op=MPI.SUM)
-assert total == 2
-print("MPI test passed!")
